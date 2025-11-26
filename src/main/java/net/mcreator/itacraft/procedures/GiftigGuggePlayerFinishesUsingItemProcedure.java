8864daa1adb59c6b697f8223a86af4b7ab092950
JahBoiMat <matveg2911@gmail.com>
2025-11-26 21:47:27 +0100

    oscinerusecmurscoe

--- src/main/java/net/mcreator/itacraft/procedures/GiftigGuggePlayerFinishesUsingItemProcedure.java
+++ src/main/java/net/mcreator/itacraft/procedures/GiftigGuggePlayerFinishesUsingItemProcedure.java
@@ -1,13 +1,24 @@
 package net.mcreator.itacraft.procedures;
 
-import net.neoforged.bus.api.Event;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.AdvancementHolder;
+
+import net.mcreator.itacraft.ItacraftMod;
 
 public class GiftigGuggePlayerFinishesUsingItemProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
 		if (entity instanceof ServerPlayer _player && _player.level() instanceof ServerLevel _level) {
-			AdvancementHolder _adv = _level.getServer().getAdvancements().get(ResourceLocation.parse("itacraft:deleted_mod_element"));
+			AdvancementHolder _adv = _level.getServer().getAdvancements().get(ResourceLocation.parse("itacraft:spiste_gugge"));
 			if (_adv != null) {
 				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
 				if (!_ap.isDone()) {
