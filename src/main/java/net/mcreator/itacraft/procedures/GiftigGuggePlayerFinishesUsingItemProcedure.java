6b23e3fd51661ddce6ad98417cfbc88548814c49
JahBoiMat <matveg2911@gmail.com>
2025-11-26 21:46:18 +0100

    giftig neger gugge

---
+++ src/main/java/net/mcreator/itacraft/procedures/GiftigGuggePlayerFinishesUsingItemProcedure.java
@@ -0,0 +1,34 @@
+package net.mcreator.itacraft.procedures;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.AdvancementHolder;
+
+import net.mcreator.itacraft.ItacraftMod;
+
+public class GiftigGuggePlayerFinishesUsingItemProcedure {
+	public static void execute(LevelAccessor world, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof ServerPlayer _player && _player.level() instanceof ServerLevel _level) {
+			AdvancementHolder _adv = _level.getServer().getAdvancements().get(ResourceLocation.parse("itacraft:spiste_gugge"));
+			if (_adv != null) {
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					for (String criteria : _ap.getRemainingCriteria())
+						_player.getAdvancements().award(_adv, criteria);
+				}
+			}
+		}
+		ItacraftMod.queueServerWork(60, () -> {
+			entity.hurt(new DamageSource(world.holderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.parse("itacraft:forgiftet")))), 100);
+		});
+	}
+}
