afd53e4da57034f1c28b4df0eade3c2553028acb
Cyberhansen <mathiashansen2007@gmail.com>
2025-11-26 18:40:37 +0100

    vippsbank oppsettmango ikke ferdig

--- src/main/java/net/mcreator/itacraft/item/HawkPhoneItem.java
+++ src/main/java/net/mcreator/itacraft/item/HawkPhoneItem.java
@@ -13,8 +13,8 @@ import net.minecraft.core.BlockPos;
 
 public class HawkPhoneItem extends Item {
 	public HawkPhoneItem(Item.Properties properties) {
-		super(properties.durability(100).attributes(ItemAttributeModifiers.builder().add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 1, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
-				.add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -3, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build()).enchantable(1));
+		super(properties.durability(100).fireResistant().attributes(ItemAttributeModifiers.builder().add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 3, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
+				.add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -3, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build()).enchantable(2));
 	}
 
 	@Override
@@ -31,10 +31,5 @@ public class HawkPhoneItem extends Item {
 	@Override
 	public void hurtEnemy(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
 		itemstack.hurtAndBreak(2, entity, LivingEntity.getSlotForHand(entity.getUsedItemHand()));
-	}
-
-	@Override
-	public boolean isFoil(ItemStack itemstack) {
-		return true;
 	}
 }
