5cc5ea2837bd46b37ee3ddc9db6f00bc95ee36c9
JahBoiMat <matveg2911@gmail.com>
2025-11-26 18:44:53 +0100

    NEGRE

--- src/main/java/net/mcreator/itacraft/client/renderer/GamerGirlRenderer.java
+++ src/main/java/net/mcreator/itacraft/client/renderer/GamerGirlRenderer.java
@@ -1,5 +1,13 @@
 package net.mcreator.itacraft.client.renderer;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+
+import net.mcreator.itacraft.entity.GamerGirlEntity;
+import net.mcreator.itacraft.client.model.ModelGamerGirl;
+
 public class GamerGirlRenderer extends MobRenderer<GamerGirlEntity, LivingEntityRenderState, ModelGamerGirl> {
 	private GamerGirlEntity entity = null;
 
