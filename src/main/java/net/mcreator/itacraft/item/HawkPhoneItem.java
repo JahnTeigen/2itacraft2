f099e960cc35df4339da0b8d53a587c3d57bdc5e
JahnTeigen <bpfredrik@gmail.com>
2025-11-28 19:25:49 +0100

    welcome to drammen kommune

--- src/main/java/net/mcreator/itacraft/item/HawkPhoneItem.java
+++ src/main/java/net/mcreator/itacraft/item/HawkPhoneItem.java
@@ -1,5 +1,21 @@
 package net.mcreator.itacraft.item;
 
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.itacraft.procedures.OpenHawkPhoneProcedure;
+
 public class HawkPhoneItem extends Item {
 	public HawkPhoneItem(Item.Properties properties) {
 		super(properties.durability(100).fireResistant().attributes(ItemAttributeModifiers.builder().add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 3, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
@@ -25,7 +41,7 @@ public class HawkPhoneItem extends Item {
 	@Override
 	public InteractionResult use(Level world, Player entity, InteractionHand hand) {
 		InteractionResult ar = super.use(world, entity, hand);
-		OpenHawkPhoneProcedure.execute();
+		OpenHawkPhoneProcedure.execute(entity);
 		return ar;
 	}
 }
