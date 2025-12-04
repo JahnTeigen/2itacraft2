88eb8afbe337f242647d0890d2ee209c6851ba15
JahnTeigen <bpfredrik@gmail.com>
2025-12-04 19:20:20 +0100

    Integrated Judaism simulation into the game

--- src/main/java/net/mcreator/itacraft/block/EkteGummitrePressurePlateBlock.java
+++ src/main/java/net/mcreator/itacraft/block/EkteGummitrePressurePlateBlock.java
@@ -1,6 +1,12 @@
 package net.mcreator.itacraft.block;
 
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.PressurePlateBlock;
 
 public class EkteGummitrePressurePlateBlock extends PressurePlateBlock {
 	public EkteGummitrePressurePlateBlock(BlockBehaviour.Properties properties) {
